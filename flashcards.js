151701f475a06536c44609b6d84f28ce3b9f4206
kabalab <kaleb@theflings.com>
2026-09-09 14:26:44 -0700

    Added css and js file for flashcards.html refined the script for flashcards changed name for css file on the homepage and added a js file for it added uniform spacing for all the files and added all units to temp values in cards.json. Has one error where when starting on asl start side it will flip on refresh or start sometimes

---
+++ flashcards.js
@@ -0,0 +1,116 @@
+let cardList = [];
+let backstart = false;
+const flipe = document.getElementById("flipe");
+let currentCard = 0;
+const cardi = document.getElementById("cardi");
+const front = document.getElementById("front");
+const back = document.getElementById("back");
+const front_number = document.getElementById("front-number");
+const back_number = document.getElementById("back-number");
+
+function flip(element){
+    element.classList.toggle("flipped");
+}
+
+async function fetchCards(){
+    const response = await fetch("./cards.json");
+    const data = await response.json();
+    const units = (new URLSearchParams(window.location.search).get("units") ?? "100").split(",").map(Number);
+    const side = (new URLSearchParams(window.location.search).get("side") ?? "0");
+    if (side === "0") {
+        cardi.style.transition = "none";
+        cardi.classList.add("flipped");
+        requestAnimationFrame(() => {
+            cardi.style.transition = "transform 0.8s";
+        });
+        flipe.textContent = "Start on Term";
+        backstart = true;
+    }
+
+
+    for (let i = 1;i <= 30;i++){
+        if (units.includes(i)){
+            cardList.push(...Object.values(data[`Unit${i}`]))
+        }
+    }
+
+    shuffle();
+    showCard();
+}
+
+function backflip(){
+    backstart = !backstart;
+    if (backstart) {
+        flipe.textContent = "Start on Term";
+        cardi.classList.add("flipped");
+    }
+    else {
+        flipe.textContent = "Start on Answer";
+        cardi.classList.remove("flipped");
+    }
+}
+
+function showCard() {
+    front.lastChild.textContent = cardList[currentCard].front;
+    back.lastChild.textContent = cardList[currentCard].back;
+    front_number.textContent = currentCard + 1;
+    back_number.textContent = currentCard + 1;
+}
+
+function nextCard(){
+    if (currentCard < cardList.length - 1) {
+    currentCard++;
+    } else {
+    currentCard = 0;
+    }
+    cardi.style.transition = "none";
+    if (!backstart){
+    cardi.classList.remove("flipped");
+    }
+    else {
+    cardi.classList.add("flipped");
+    }
+    requestAnimationFrame(() => {
+    cardi.style.transition = "transform 0.8s";
+    });
+    showCard();
+}
+
+async function previousCard(){
+    if (currentCard > 0) {
+    currentCard--;
+    } else {
+    currentCard = cardList.length - 1;
+    }
+    cardi.style.transition = "none";
+    if (!backstart){
+    cardi.classList.remove("flipped");
+    }
+    else {
+    cardi.classList.add("flipped");
+    }
+    requestAnimationFrame(() => {
+    cardi.style.transition = "transform 0.8s";
+    });
+    showCard();
+}
+
+function shuffle(){
+    for (let i = cardList.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [cardList[i], cardList[j]] = [cardList[j], cardList[i]];
+    }
+    currentCard = 0;
+    cardi.style.transition = "none";
+    if (backstart){
+        cardi.classList.add("flipped");
+    } else {
+        cardi.classList.remove("flipped");
+    }
+    requestAnimationFrame(() => {
+    cardi.style.transition = "transform 0.8s";
+    });
+    showCard();
+}
+
+fetchCards();
---
+++ homePage.js
@@ -0,0 +1,86 @@
+const mainDiv = document.querySelector(".divButtons");
+let unitsSelected = [];
+
+setUpUnits();
+
+function setUpUnits(){
+    
+    for (let i = 1; i <= 30; i++){
+        const currentButton = document.createElement("button");
+        currentButton.className = "unitButton buttons";
+        currentButton.id = `unit${i}`;
+        currentButton.textContent = `${i}`;
+        currentButton.dataset.uniton = "true";
+        currentButton.onclick = function() {decideColorOfButton(`unit${i}`);};
+        mainDiv.appendChild(currentButton);
+    }
+}
+
+function decideColorOfButton(buttonId){
+    const myButt = document.getElementById(buttonId);
+    if (myButt.dataset.uniton === "true"){
+        myButt.style.backgroundColor = "gray";
+        myButt.dataset.uniton = "false";
+        if (buttonId != "side"){
+            unitsSelected.push(myButt);
+        }
+    }
+    else {
+        myButt.style.backgroundColor = "white";
+        myButt.dataset.uniton = "true";
+        if (buttonId != "side"){
+            unitsSelected.splice(unitsSelected.indexOf(myButt),1);
+        }
+    }
+}
+
+function decideImageOfButton(buttonId){
+    const myButt = document.getElementById(buttonId);
+    const myButtIcon = myButt.querySelector('img')
+    if (myButt.dataset.uniton === "true"){
+        myButtIcon.src = "media/ENG.svg"
+        myButt.dataset.uniton = "false";
+    }
+    else {
+        myButt.dataset.uniton = "true";
+        myButtIcon.src = "media/ASL.svg"
+    }
+}
+
+function openSelected(){
+    if (unitsSelected.length != 0){
+        window.location.href = setUpLink();
+    } else {
+        alert("Not enough units to open");
+    }
+}
+
+function copySelected(){
+    if (unitsSelected.length != 0){
+        navigator.clipboard.writeText(setUpLink());
+        alert(`Copied "${setUpLink()}" to your clipboard`);
+    } else {
+        alert("Not enough units to copy");
+    }
+}
+
+function setUpLink(){
+    let link = "flashcards.html?";
+    if (document.getElementById("side").dataset.uniton === "false"){
+        link += "side=1&units=";
+    } else {
+        link += "side=0&units="
+    }
+
+    for (let i = 0;i < unitsSelected.length;i++){
+        link += unitsSelected[i].id.slice(4) + ((i === unitsSelected.length - 1) ? "" : ",");
+    }
+
+    return link;
+}
+
+function clearUnits(){
+    unitsSelected.slice().forEach(button => {
+        decideColorOfButton(button.id);
+    });
+}
