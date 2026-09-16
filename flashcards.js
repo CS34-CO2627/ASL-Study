1cbd8a833510c38a5edd8db4069b1a10d9eb7bab
Vidy2 <111710984+Vidy2@users.noreply.github.com>
2026-09-16 12:22:19 -0700

    Just for the big boss man guy person man woman camera tv

--- flashcards.js
+++ flashcards.js
@@ -58,11 +58,18 @@ function backflip(){
 			cardit.classList.add("flipped");
 			whichFront.dataset.aslorenglish = "eng";
 			whichFrontImg.src = "media/ENG.svg";
+            const url = new URL(window.location.href);
+            url.searchParams.set("front", "eng");
+            window.history.replaceState(null, "", url.pathname + url.search + url.hash);
     } else {
 			// Start on ASL
 			cardit.classList.remove("flipped");
 			whichFront.dataset.aslorenglish = "asl";
 			whichFrontImg.src = "media/ASL.svg";
+            const url = new URL(window.location.href);
+            url.searchParams.set("front", "asl");
+            window.history.replaceState(null, "", url.pathname + url.search + url.hash);
+
     }
 }
 
@@ -162,7 +169,7 @@ function shuffle(){
 }
 
 // I'm so glad JS uses a mathematically inaccurate modulo %
-// smh
+// smh (For Kaleb, this is shaking my head)
 function trueMod(a,b){
 	return ((a % b) + b) % b;
 }
